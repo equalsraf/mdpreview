a2e91b2d7938802b18838841bf7775c99b2a4d6b
Rui Abreu Ferreira <raf-ep@gmx.com>
2013-03-27 09:52:24 +0000

    Implement mdpreview in Python
    - It seems libmarkdown is missing for some Linux distributions, so I've
      reimplemented mdpreview in Python2 - using PySide and python-markdown.

---
+++ mdpreview.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+A markdown live preview
+"""
+
+from PySide.QtGui import QApplication
+from PySide.QtWebKit import QWebView
+from PySide.QtCore import QFileSystemWatcher, QFileInfo
+import sys
+import markdown
+
+class MarkdownView(QWebView):
+
+    def __init__(self, path, parent=None, encoding='utf8'):
+        QWebView.__init__(self, parent)
+        self.path = path
+        self.encoding = encoding
+        self.__watcher = QFileSystemWatcher(self)
+
+        self.__watcher.fileChanged.connect(self.loadFile)
+        self.__watcher.directoryChanged.connect(self.loadFile)
+
+        self.__watcher.addPath(self.path)
+        self.__watcher.addPath(QFileInfo(self.path).path())
+
+        self.setWindowTitle(self.path)
+
+        self.loadFile()
+
+    def loadFile(self):
+        if not QFileInfo(self.path).exists():
+            return
+
+        inp = open(self.path).read().decode(self.encoding)
+        self.setHtml(markdown.markdown(inp, extensions=['meta', 'codehilite']))
+
+app = QApplication(sys.argv)
+
+if len(app.arguments()) != 2:
+    print('Usage: mdpreview <path>')
+    sys.exit(-1)
+
+win = MarkdownView(app.arguments()[1])
+
+win.show()
+sys.exit(app.exec_())
+
